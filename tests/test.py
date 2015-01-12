f79ff2de4bdb875349385e9b82cdd2e90abd8519
Samarjeet <singh.samarjeet.27@gmail.com>
2015-01-12 18:34:22 +0530

    Add travis build

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import urllib2
 
@@ -9,13 +10,19 @@ def url_valid(x):
 		return False
 
 def main():
-	j = open('directory.json').read()
+	j = open('./directory.json').read()
 	obj = json.loads(j)
-	
+	invalid_urls = []
 	for x in obj:
 		url = obj[x]
 		if not url_valid(url):
-			print "%s is invalid" %url
+			invalid_urls.append(url)
+
+	if len(invalid_urls) > 0:
+		print '\n'.join(invalid_urls) + " are invalid urls"
+		sys.exit(1)
+	else:
+		sys.exit(0)
 
 if __name__ == '__main__':
 	main()
